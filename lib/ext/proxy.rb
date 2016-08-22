9293b3f10ae8a4acdc877c29c60409c1280ad972
DAIKI INABA <daiki@DAIKI-no-MacBook-Pro.local>
2016-08-22 20:31:46 +0900

    add proxy

---
+++ lib/ext/proxy.rb
@@ -0,0 +1,10 @@
+module Line
+  module Bot
+    class HTTPClient
+      def post(url, payload, header = {})
+        RestClient.proxy = ENV["FIXIE_URL"]
+        RestClient.post(url, payload, header)
+      end
+    end
+  end
+end
