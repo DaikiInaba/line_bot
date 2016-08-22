a82b61b6885cfef396a1bdc4ee7cb7fca58dd665
DaikiInaba <ma1da0na4i3@gmail.com>
2016-08-23 01:10:56 +0900

    add proxy

--- lib/line/httpclient.rb
+++ lib/line/httpclient.rb
@@ -3,17 +3,10 @@ require 'rest-client'
 module Line
   module Bot
     class HTTPClient
-
-      # @return [Net::HTTP]
-      def http(uri)
-        http = Net::HTTP::Proxy(ENV["FIXIE_URL"])
-        http = http.new(uri.host, uri.port)
-        http.set_debug_output(Rails.logger)
-        if uri.scheme == "https"
-          http.use_ssl = true
-        end
-
-        http
+      def post(url, payload, header = {})
+        Rails.logger.error("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+        RestClient.proxy = ENV["FIXIE_URL"]
+        RestClient.post(url, payload, header)
       end
     end
   end
