32930d74fb209cdeeeb11777ca70c7f1c44d3cad
DaikiInaba <ma1da0na4i3@gmail.com>
2016-08-23 01:36:25 +0900

    a

--- config/initializers/line-bot-api-proxy.rb
+++ config/initializers/line-bot-api-proxy.rb
@@ -3,9 +3,8 @@ require 'rest-client'
 module Line
   module Bot
     class HTTPClient
-      def post(url, payload, header = {})
-        RestClient.proxy = ENV["FIXIE_URL"]
-        RestClient.post(url, payload, header)
+      def post
+        p "test"
       end
     end
   end
