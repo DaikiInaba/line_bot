95308af9b3f701b7d069a8b5cdaf867e46182c90
DaikiInaba <ma1da0na4i3@gmail.com>
2016-08-23 01:02:47 +0900

    add proxy

--- lib/line/httpclient.rb
+++ lib/line/httpclient.rb
@@ -8,16 +8,12 @@ module Line
       def http(uri)
         http = Net::HTTP::Proxy(ENV["FIXIE_URL"])
         http = http.new(uri.host, uri.port)
+        http.set_debug_output(Rails.logger)
         if uri.scheme == "https"
           http.use_ssl = true
         end
 
         http
-      end
-
-      def post(url, payload, header = {})
-        uri = URI(url)
-        http(uri).post(uri.request_uri, payload, header)
       end
     end
   end
