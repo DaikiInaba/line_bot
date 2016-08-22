37931f2592fc65c96f02d9a8b35c2c901fbbf7e5
DAIKI INABA <daiki@DAIKI-no-MacBook-Pro.local>
2016-08-22 19:18:46 +0900

    mod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  require 'line/api'
 
   def callback
+    logger.debug(ENV["LINE_CHANNEL_ID"])
+    logger.debug(ENV["LINE_CHANNEL_SECRET"])
+    logger.debug(ENV["LINE_CHANNEL_MID"])
+
     client = Line::Bot::Client.new { |config|
       config.channel_id = ENV["LINE_CHANNEL_ID"]
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_mid = ENV["LINE_CHANNEL_MID"]
     }
+
 
     Rails.logger.debug(client)
 
