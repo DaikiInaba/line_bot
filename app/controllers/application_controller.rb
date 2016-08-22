940ee8b410bc6307e4472bd0690bb49a4f3608f7
DAIKI INABA <daiki@DAIKI-no-MacBook-Pro.local>
2016-08-22 19:04:21 +0900

    remove private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
   def callback
     client = Line::Bot::Client.new { |config|
       config.channel_id = ENV["LINE_CHANNEL_ID"]
