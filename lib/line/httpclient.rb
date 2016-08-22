ba90cc6873635597587a4027df84f9662291500b
DaikiInaba <ma1da0na4i3@gmail.com>
2016-08-23 00:50:25 +0900

    add proxy

--- lib/line/httpclient.rb
+++ lib/line/httpclient.rb
@@ -3,9 +3,21 @@ require 'rest-client'
 module Line
   module Bot
     class HTTPClient
+
+      # @return [Net::HTTP]
+      def http(uri)
+        http = Net::HTTP::Proxy(ENV["FIXIE_URL"])
+        http = http.new(uri.host, uri.port)
+        if uri.scheme == "https"
+          http.use_ssl = true
+        end
+
+        http
+      end
+
       def post(url, payload, header = {})
-        # RestClient.proxy = ENV["FIXIE_URL"]
-        # RestClient.post(url, payload, header)
+        uri = URI(url)
+        http(uri).post(uri.request_uri, payload, header)
       end
     end
   end
