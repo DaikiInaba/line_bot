df22517f8e4acc72dc77123e1188e06d105df225
DaikiInaba <ma1da0na4i3@gmail.com>
2016-08-23 00:37:08 +0900

    add proxy

--- lib/line/httpclient.rb
+++ lib/line/httpclient.rb
@@ -4,9 +4,8 @@ module Line
   module Bot
     class HTTPClient
       def post(url, payload, header = {})
-        test
-        RestClient.proxy = ENV["FIXIE_URL"]
-        RestClient.post(url, payload, header)
+        # RestClient.proxy = ENV["FIXIE_URL"]
+        # RestClient.post(url, payload, header)
       end
     end
   end
