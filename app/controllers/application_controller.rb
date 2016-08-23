07eec94433ba4d8316ef82f7d54dbb0d3da1165d
DaikiInaba <ma1da0na4i3@gmail.com>
2016-08-23 16:36:28 +0900

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,10 @@ class ApplicationController < ActionController::Base
     end
 
     receive_request = Line::Bot::Receive::Request.new(request.env)
-    receive_request.data.each do |message|
-      processor = Line::Bot::Processor.new(client, message)
+    receive_request.data.each do |data|
+      Rails.logger.debug(data)
+      Rails.logger.debug(data.content)
+      processor = Line::Bot::Processor.new(client, data)
       processor.process
     end
     render nothing: true
