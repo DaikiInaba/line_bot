1a9061f159adb47bec4b3bf5b88b216dd58dea7c
DaikiInaba <ma1da0na4i3@gmail.com>
2016-08-24 18:37:41 +0900

    send message to all people

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
     receive_request = Line::Bot::Receive::Request.new(request.env)
     receive_request.data.each do |data|
       Rails.logger.debug("==========================================")
-      Rails.logger.debug(data.content[:originalContentUrl])
+      Rails.logger.debug(data.content)
       Rails.logger.debug("==========================================")
       processor = Line::Bot::Processor.new(client, data)
       processor.process
