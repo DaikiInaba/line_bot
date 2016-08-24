f5bf5fb97657e68741db9d462d4d64eb63805153
DaikiInaba <ma1da0na4i3@gmail.com>
2016-08-24 17:49:08 +0900

    send message to all people

--- config/initializers/processor.rb
+++ config/initializers/processor.rb
@@ -51,7 +51,8 @@ module Line
         user.save!
 
         mids = User.all.map{|user| user.mid}
-        mids = mids.delete!(from_mid)
+        deleted_mids = mids.delete(from_mid)
+        mids = deleted_mids if deleted_mids
 
         mids
       end
