e664448a5f72ec71786ea900546a5f54a45079db
DaikiInaba <ma1da0na4i3@gmail.com>
2016-08-24 14:13:47 +0900

    test

--- config/initializers/processor.rb
+++ config/initializers/processor.rb
@@ -59,22 +59,22 @@ module Line
       end
 
       def text_processor
-        message = ""
-        text = data.content[:text]
+        # message = ""
+        # text = data.content[:text]
 
-        if /(?<month>\d{1,2})月(?<date>\d){1,2}日/ =~ text
-          year = Date.today.to_s
-          date = year + month + date
-          events = Event.where('started_at <= ?', date).where('expired_at >= ?', date)
-          return "#{month}月#{date}日に開催しているイベントはないみたいです..." if event.length == 0
+        # if /(?<month>\d{1,2})月(?<date>\d){1,2}日/ =~ text
+        #   year = Date.today.to_s
+        #   date = year + month + date
+        #   events = Event.where('started_at <= ?', date).where('expired_at >= ?', date)
+        #   return "#{month}月#{date}日に開催しているイベントはないみたいです..." if event.length == 0
 
-          message += "#{month}月#{date}日に開催しているイベントは"
-          events.each do |event|
-            message += "\n---------------------------------------"
-            message += "\n#{event.name}"
-            message += "\n#{event.event_url}"
-          end
-          messgage += "です"
+        #   message += "#{month}月#{date}日に開催しているイベントは"
+        #   events.each do |event|
+        #     message += "\n---------------------------------------"
+        #     message += "\n#{event.name}"
+        #     message += "\n#{event.event_url}"
+        #   end
+        #   messgage += "です"
         end
       end
     end
