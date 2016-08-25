478dc33bf32f0ede3bac1803256673dd724daf5a
DaikiInaba <ma1da0na4i3@gmail.com>
2016-08-25 21:30:41 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825053537) do
+ActiveRecord::Schema.define(version: 20160825113831) do
 
   create_table "bot_messages", force: :cascade do |t|
     t.text     "text",       limit: 65535
     t.integer  "stage",      limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.text     "image_url",  limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
